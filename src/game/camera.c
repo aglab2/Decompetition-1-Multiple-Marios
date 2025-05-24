10125cd9ad5cd6659d6b6a60d4a66848f5adbd33
aglab2 <aglab3@gmail.com>
2025-05-24 17:14:56 +0800

    Added basic OW

--- src/game/camera.c
+++ src/game/camera.c
@@ -6068,6 +6068,9 @@ struct CameraTrigger sCamBBH[] = {
 struct CameraTrigger sCamExample[] = {
 	NULL_TRIGGER
 };
+struct CameraTrigger sCamCastleGrounds[] = {
+	NULL_TRIGGER
+};
 struct CameraTrigger *sCameraTriggers[LEVEL_COUNT + 1] = {
     NULL,
     #include "levels/level_defines.h"
