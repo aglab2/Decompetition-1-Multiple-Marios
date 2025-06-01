f78aff2b4e7d90535e8bb515694ff5e08111bf3a
aglab2 <aglab3@gmail.com>
2025-06-01 18:29:47 +0800

    Fixed seams on bg

--- levels/example/ma_bg/header.h
+++ levels/example/ma_bg/header.h
@@ -1,6 +1,6 @@
 extern u8 ma_bg_prnbg8_1_rgba16[];
 extern u8 ma_bg_prnbg_2_rgba16[];
 extern Vtx ma_bg_object_00AF785C_mesh_vtx_cull[8];
-extern Vtx ma_bg_object_00AF785C_mesh_vtx_0[96];
-extern Vtx ma_bg_object_00AF785C_mesh_vtx_1[1168];
+extern Vtx ma_bg_object_00AF785C_mesh_vtx_0[92];
+extern Vtx ma_bg_object_00AF785C_mesh_vtx_1[1080];
 extern Gfx ma_bg_object_00AF785C_mesh[];
